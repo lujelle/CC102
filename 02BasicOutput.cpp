e07b3e2b34df1c0b3193659bb1be63528a4c1c98
Lujelle Furiscal <lujelle496@gmail.com>
2024-10-03 09:55:22 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	 cout << "Our Lady Of Lourdes College Foundation" << endl;
+	 cout << "Computer Science Technology" << endl;
+	 cout << "Bachelor Science Information Technology" << endl;
+	 cout << "Lujelle P. Furiscal" << endl;
+	 cout << "P-6 SanRafael Jose Panganiban Camarines Norte" << endl;
+	 return 0;
+	 
+}
