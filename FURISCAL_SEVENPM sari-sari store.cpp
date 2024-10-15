193322784d366581dae3e0f9758807564d48f461
Lujelle Furiscal <lujelle496@gmail.com>
2024-10-15 10:07:11 +0800

    Add files via upload

---
+++ FURISCAL_SEVENPM sari-sari store.cpp
@@ -0,0 +1,40 @@
+#include <iostream> 
+
+using namespace std;
+
+int main()
+{
+	
+	 string sugar,rice,sardines,coffee,Milk ;
+	 float sugarprice,riceprice,sardinesprice,coffepriceUSD,milkprice ;
+	 int sugarQty,riceQty,sardinesQty,coffeQty,milkQty ;
+	 
+	cout << "Enter the sugarPriceUsd";
+	cin >> sugar;
+	cout << "Enter the ricePriceUsd";
+	cin >> rice;
+	cout << "Enter the sardinesPriceUsd";
+	cin >> sardines;
+	cout << "Enter the milkPriceUsd";
+	cin >> milk;
+	 
+	 usdToPhp
+	 poundToPhp
+	 
+	cout << "Quantity of product Purchased";
+	cin >> sugarQty,riceQty,sardinesQty,coffeQty,milkQty;
+	sugarPriceUSD sugarQty * pondtoUsd
+	ricePriceUSD riceQty * poundtoUSD
+	sardinesPriceUsd sardinesQty * poundtoUSD
+	coffePriceUSD CoffeQty * poundtoUSD
+	milkPriceUSD milkQty * poundtoUSD
+	cout << "Price per item in PHP:\n" << sugarPriceUSD << endl;
+	totalCostPhp = sugarQty * usdToPhp;
+	totalCostPhp = riceQty * poundToPhp;
+	totalCostPhp = sardinesQty * poundToPhp;
+	totalCostPhp =coffeeQty * poundToPhp;
+	totalCostPhp = milkQty * poundToPhp;
+	cout << "Total cost per item in PHP:\n" << totalCostPhp << endl;
+	return 0;
+	
+}
